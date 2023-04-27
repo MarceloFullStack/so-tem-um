731fa6f46fa2a27d4d1058a1e600c44633f700ef
Marcelo Guimarães <fotofacil@live.com>
2023-04-27 09:14:46 -0300

    refactor
    mudanca de titulo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <nav className="fixed left-0 right-0 top-0 flex items-center justify-center bg-slate-800 p-6">
       <div className="text-2xl font-extrabold text-slate-50 uppercase">
-        Só tem 1
+        Só Tem Um 
       </div>
     </nav>
   )
